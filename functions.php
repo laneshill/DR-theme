e459ca5ede735a3b3e1b70029efca357382b824c
sean <sean@ubuntu.ubuntu-domain>
2011-07-13 12:43:37 -0500

    menu colors and height fixed

--- functions.php
+++ functions.php
@@ -506,3 +506,11 @@ function twentyten_posted_in() {
 	);
 }
 endif;
+
+
+function add_first_and_last($output) {
+  $output = preg_replace('/class="menu-item/', 'class="first-menu-item menu-item', $output, 1);
+  $output = substr_replace($output, 'class="last-menu-item menu-item', strripos($output, 'class="menu-item'), strlen('class="menu-item'));
+  return $output;
+}
+add_filter('wp_nav_menu', 'add_first_and_last');
